8f1e6e2f29da0590a758424edec0e02607c6a4c0
nethangabrielb <bagasbas.nethangabriel@gmail.com>
2026-04-06 13:55:39 +0800

    feat: extend skeleton rows in ApplicationsTableSkeleton for improved loading representation

--- src/components/applications-table-skeleton.tsx
+++ src/components/applications-table-skeleton.tsx
@@ -10,7 +10,18 @@ import {
 } from "@/components/ui/table";
 
 const ApplicationsTableSkeleton = () => {
-  const skeletonRows = ["row-1", "row-2", "row-3", "row-4", "row-5"];
+  const skeletonRows = [
+    "row-1",
+    "row-2",
+    "row-3",
+    "row-4",
+    "row-5",
+    "row-6",
+    "row-7",
+    "row-8",
+    "row-9",
+    "row-10",
+  ];
 
   return (
     <Table>
