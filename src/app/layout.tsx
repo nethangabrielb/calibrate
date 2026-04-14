923357af2e457e555b17a84df53b9635fae0a994
nethangabrielb <bagasbas.nethangabriel@gmail.com>
2026-04-14 23:43:09 +0800

    fix: add NextTopLoader for improved loading experience and update package dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import NextTopLoader from "nextjs-toploader";
 
 import { Geist, Geist_Mono } from "next/font/google";
 import { headers } from "next/headers";
@@ -44,6 +45,17 @@ export default async function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <NextTopLoader
+          color="oklch(0.589 0.115 184.704)"
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={3}
+          crawl={true}
+          showSpinner={false}
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px oklch(0.589 0.115 184.704), 0 0 5px oklch(0.589 0.115 184.704)"
+        />
         <Toaster richColors position="top-right" theme="light" />
         {/* Sidebar here only visible for authenticated users */}
         <main className="flex flex-col min-h-svh w-full overflow-hidden lg:flex-row">
