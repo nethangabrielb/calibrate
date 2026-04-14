0098099890f1699bdea2adc160bff0f80801067b
nethangabrielb <bagasbas.nethangabriel@gmail.com>
2026-04-14 10:15:38 +0800

    feat: update Dashboard layout for improved styling and structure

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 const Dashboard = () => {
   return (
-    <div className="flex h-screen w-full items-center justify-center">
+    <div className="flex w-full flex-col gap-4 bg-background px-8 py-4 text-foreground">
       <h1 className="text-2xl font-bold">Welcome to the Dashboard!</h1>
     </div>
   );
